437690cced50b230ce5ef6b8bbe9d69737b0e146
simaanc <simaanc@gmail.com>
2023-08-29 22:53:22 +0000

    Added packet type detection and checksum

--- app.py
+++ app.py
@@ -16,9 +16,11 @@ configLocation = str(source_dir) + "/configfile.ini"
 # Open a serial port on the second FTDI device interface (IF/2) @ 115200 baud
 port = pyftdi.serialext.serial_for_url("ftdi://ftdi:232:/1", baudrate=115200)
 
+
 def write_file():
     with open(configLocation, "w") as configfile:
         config.write(configfile)
+
 
 if not os.path.exists(configLocation):
     config["ServerConf"] = {
@@ -32,13 +34,27 @@ else:
     config.read(configLocation)
     print(config.sections())
 
+
+def calculate_checksum(data):
+    checksum = 0xFF - (sum(data) & 0xFF)
+    return checksum & 0xFF
+
+
+def validate_checksum(data):
+    calculated_checksum = calculate_checksum(
+        data[:-1]
+    )  # Exclude the last byte (received checksum)
+    received_checksum = data[-1]
+    return calculated_checksum == received_checksum
+
+
 def serial_reader():
     buffer = b""  # Buffer to hold incoming data
 
     while True:
         try:
             # Read incoming data
-            new_data = port.read(25)  # Adjust the read size as needed
+            new_data = port.read(6)  # Adjust the read size as needed
 
             if not new_data:
                 continue
@@ -50,52 +66,94 @@ def serial_reader():
 
                 if start_idx == -1:
                     buffer = b""  # Discard incomplete packets
-                    break            # If the start delimiter is not at the beginning, discard bytes before it
+                    break
                 if start_idx > 0:
                     buffer = buffer[start_idx:]
-                # Check if there's enough data for a complete packet
-                if len(buffer) < 20:  # Adjust based on packet structure
+
+                # Check if there's enough data for MSB, LSB, and checksum
+                if len(buffer) < 5:  # Minimum length for MSB, LSB, and checksum
                     break
+
+                # Extract MSB and LSB to calculate packet length
+                msb = int(buffer[1:2].hex(), 16)
+                lsb = int(buffer[2:3].hex(), 16)
+                packet_length = (msb << 8) | lsb
+
+                # Check if there's enough data for the complete packet
+                if (
+                    len(buffer) < packet_length + 4
+                ):  # 4 bytes for MSB, LSB, and checksum
+                    break
+
                 # Extract and process the complete packet
-                complete_packet = buffer[:20]  # Adjust based on packet structure
-                buffer = buffer[20:]  # Remove the processed packet from the buffer
+                complete_packet = buffer[: packet_length + 4]
+                buffer = buffer[packet_length + 4 :]
 
-                # Process the packet as before
-                hex_representation = complete_packet.hex()
+                # Extract the frame type (4th byte)
+                frame_type = complete_packet[3]
+                # print("Frame Type:", hex(frame_type))
 
-                hex_representation = hex_representation.upper()
+                # Extract the 64-bit source address (next 8 bytes)
+                source_address_64 = complete_packet[4:12]
+                # print("Source Address (64-bit):", source_address_64.hex())
 
-                # Interpret the API frame structure
-                start_delimiter = hex_representation[0:2]
-                length = hex_representation[2:6]
-                frame_type = hex_representation[6:8]
-                source_address_64 = hex_representation[8:24]
-                source_address_16 = hex_representation[24:28]
-                receive_options = hex_representation[28:30]
-                data_hex = hex_representation[30:38]
-                checksum_hex = hex_representation[38:40]
+                # Extract the 16-bit source network address (next 2 bytes)
+                source_address_16 = complete_packet[12:14]
+                # print("Source Address (16-bit):", source_address_16.hex())
 
-                # Convert data to ASCII
-                data_ascii = bytes.fromhex(data_hex).decode("utf-8", errors="replace")
+                # Extract the receive options (next 1 byte)
+                receive_options = complete_packet[14]
+                # print("Receive Options:", hex(receive_options))
 
-                current_time = datetime.datetime.now(datetime.timezone.utc)
-                date_time_str = current_time.isoformat()
+                # Calculate the number of bytes for the received data
+                received_data_length = (
+                    packet_length - 12
+                )  # Subtracting 8 bytes for address, 2 bytes for network address, and 1 byte for options
 
-                # Filter out non-ASCII characters from data_ascii
-                data_ascii = "".join(char for char in data_ascii if char.isascii())
+                # Extract the received data
+                received_data = complete_packet[15 : 15 + received_data_length]
+                # print("Received Data (Hex):", received_data.hex())
 
-                global current_value
-                current_value = data_ascii
+                # Convert received data from hex to ASCII
+                received_data_ascii = bytes.fromhex(received_data.hex()).decode(
+                    "utf-8", errors="replace"
+                )
+                received_data_ascii = "".join(
+                    char for char in received_data_ascii if char.isascii()
+                )
+                # print("Received Data (ASCII):", received_data_ascii)
 
-                # Construct JSON payload
-                payload = {
-                    "source_address_64": source_address_64,
-                    "date_time": date_time_str,
-                    "data": data_ascii
-                }
+                # Validate the checksum
+                received_packet = complete_packet[3:]  # Exclude the start delimiter
+                is_checksum_valid = validate_checksum(received_packet)
+                if is_checksum_valid:
+                    print("Checksum is valid.")
+                else:
+                    print("Checksum is invalid. Ignoring the data.")
+                    continue
 
-                # Put the JSON payload in a queue to be processed outside this loop
-                json_payload_queue.put(payload)
+                # Process the packet based on the identified frame type
+                if frame_type == 0x90:  # Receive packet
+                    print("Received Packet 90:", complete_packet.hex())
+
+                    current_time = datetime.datetime.now(datetime.timezone.utc)
+                    date_time_str = current_time.isoformat()
+
+                    # Construct JSON payload
+                    payload = {
+                        "source_address_64": str(source_address_64.hex()).upper(),
+                        "date_time": date_time_str,
+                        "data": received_data_ascii,
+                    }
+                    print(payload)
+                    # Put the JSON payload in a queue to be processed outside this loop
+                    json_payload_queue.put(payload)
+                elif frame_type == 0x95:  # Node identification indicator
+                    print("Received Packet 95:", complete_packet.hex())
+                else:
+                    print("Unknown frame type:", hex(frame_type))
+
+                print("\n\n")
 
         except Exception as e:
             print("Exception in serial_reader:", e)
@@ -120,7 +178,7 @@ while True:
 
         headers = {
             "Authorization": f"Bearer {api_key}",
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         }
 
         post_response = requests.post(server_url, json=payload, headers=headers)
